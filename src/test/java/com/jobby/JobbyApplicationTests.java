c74cfeb47000dd21d0187d0bdaa77273b2cc6915
Gustavo Ferreira <70302908+Gussstavof@users.noreply.github.com>
2023-07-02 17:07:20 -0300

    Configuração do spring

---
+++ src/test/java/com/jobby/JobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
