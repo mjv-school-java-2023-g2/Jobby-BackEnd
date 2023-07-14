a488f7dd68284be69445a5da6cc818a277f5792b
Gustavo Ferreira <70302908+Gussstavof@users.noreply.github.com>
2023-07-14 16:54:11 -0300

    fix

--- src/main/java/com/jobby/core/models/entities/candidato/Candidato.java
+++ src/main/java/com/jobby/core/models/entities/candidato/Candidato.java
@@ -1,4 +1,12 @@
 package com.jobby.core.models.entities.candidato;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Candidato {
+    @Id
+    Integer id;
 }
--- src/main/java/com/jobby/core/repositories/persistence/CidadeRepository.java
+++ src/main/java/com/jobby/core/repositories/persistence/CidadeRepository.java
@@ -1,13 +1,8 @@
 package com.jobby.core.repositories.persistence;
 
 import com.jobby.core.models.entities.endereco.Cidade;
-import com.jobby.core.models.entities.endereco.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
-
-    Optional<Cidade> findById(Integer id);
 
 }
